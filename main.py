9cdbe06fd1362d9b4b5252385c3d506eae2f4e71
noukinouk <admin@bzh-electricite.com>
2026-03-15 00:06:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+   return {"message": "Trading scanner en ligne"}
+
+@app.get("/scan")
+def scan():
+   return {
+       "status": "ok",
+       "scanner": "actif",
+       "message": "Le scanner fonctionne"
+   }
